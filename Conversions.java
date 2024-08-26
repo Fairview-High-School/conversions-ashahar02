a35c230fa611d7cf7c39f0e969e8de1715ce1ba3
ashahar02 <ashahar02@bvsd.org>
2024-08-26 17:20:27 +0000

    11:20

--- Conversions.java
+++ Conversions.java
@@ -10,19 +10,19 @@ public class Conversions
     public static void main(String[] args)
     {
         Scanner keyboard = new Scanner(System.in);
-
-        System.out.println("1. Celsius to Fahrenheit");
-        System.out.println("2. Fahrenheit to Celsius");
-        System.out.println("3. Feet to Meters");
-        System.out.println("4. Meters to Feet");
-        System.out.println("5. Ounces to Milliliters");
-        System.out.println("6. Milliliters to Ounces");
-        //Add 2 more conversions, each both ways, to add choices 7, 8, 9, and 10.
-        //You can add even more if you would like.
-
+        System.out.println("Choose one of the options:");
+            System.out.println("1. Celsius to Fahrenheit");
+            System.out.println("2. Fahrenheit to Celsius");
+            System.out.println("3. Feet to Meters");
+            System.out.println("4. Meters to Feet");
+            System.out.println("5. Ounces to Milliliters");
+            System.out.println("6. Milliliters to Ounces");
+            System.out.println("7. Centimeters to Meters");
+            System.out.println("8. Meters to Centimeters");
+            System.out.println("9. Litters to Milliliters");
+            System.out.println("10. Milliliters to Litters");
         int selection = keyboard.nextInt();
-        keyboard.nextLine();  //go past the endline and be ready for more input
-
+        keyboard.nextLine();
         if (selection == 1)
         {
             System.out.println("Enter Celsius: ");
@@ -33,7 +33,7 @@ public class Conversions
         }
         if (selection == 2)
         {
-            //add the code to ask the user to enter Fahrenheit and then convert to Celsius
+           
         }
         //add more if statements here.
     }
