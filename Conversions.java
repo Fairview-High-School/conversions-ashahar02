ed8088809a7305fa695b0b6531a7e7e085f8df06
ashahar02 <ashahar02@bvsd.org>
2024-08-26 20:59:55 +0000

    3:00

--- Conversions.java
+++ Conversions.java
@@ -23,18 +23,86 @@ public class Conversions
             System.out.println("10. Milliliters to Litters");
         int selection = keyboard.nextInt();
         keyboard.nextLine();
+        double celsius, fahrenheit, feet ,meters, onces, milliliters, liters, centimeters;
         if (selection == 1)
         {
             System.out.println("Enter Celsius: ");
-            double celsius = keyboard.nextDouble();
+            celsius = keyboard.nextDouble();
             keyboard.nextLine();
-            double fahrenheit = celsius * (9.0 / 5.0) + 32;
-            System.out.println(celsius + " degrees celsius is " + fahrenheit + " degrees fahrenheit");
+            fahrenheit = celsius * (9.0 / 5.0) + 32;
+            System.out.println("degrees celsius is "+ celsius + " degrees fahrenheit "+ fahrenheit);
         }
         if (selection == 2)
         {
-           
+            System.out.println("Enter Fahrenheit: ");
+            fahrenheit= keyboard.nextDouble();
+            keyboard.nextLine();
+            celsius=(fahrenheit-32)/(9.0/5.0);
+            System.out.println("degrees celsius is "+ celsius + " degrees fahrenheit "+ fahrenheit);
         }
-        //add more if statements here.
+        if (selection==3)
+        {
+            System.out.println("Enter Feet: ");
+            feet = keyboard.nextDouble();
+            keyboard.nextLine();
+            meters = feet/3.281;
+            System.out.println("Lengh feet is "+ feet + " Lengh meters is "+ meters);
+        }
+        if (selection==4)
+        {
+            System.out.println("Enter meters: ");
+            meters = keyboard.nextDouble();
+            keyboard.nextLine();
+            feet = meters*3.281;
+            System.out.println("Lengh feet is "+ feet + " Lengh meters is "+ meters);
+        }
+        if (selection==5)
+        {
+            System.out.println("Enter Onces: ");
+            onces = keyboard.nextDouble();
+            keyboard.nextLine();
+            milliliters = onces*29.574            ;
+            System.out.println("Volume onces is "+ onces + " Volume milliliters is "+ milliliters);
+        }
+        if (selection==6)
+        {
+            System.out.println("Enter Milliliters: ");
+            milliliters = keyboard.nextDouble();
+            keyboard.nextLine();
+            onces = milliliters/29.547;
+            System.out.println("Volume onces is "+ onces + " Volume milliliters is "+ milliliters);
+        }
+        if (selection==7)
+        {
+            System.out.println("Enter centimeters: ");
+            centimeters = keyboard.nextDouble();
+            keyboard.nextLine();
+            meters = centimeters/100;
+            System.out.println("Lengh centimeters is "+ centimeters + " Lengh meters is "+ meters);
+        }
+        if (selection==8)
+        {
+            System.out.println("Enter meters: ");
+            meters = keyboard.nextDouble();
+            keyboard.nextLine();
+            centimeters = meters*100;
+            System.out.println("Lengh centimeters is "+ centimeters + " Lengh meters is "+ meters);
+        }
+        if (selection==9)
+        {
+            System.out.println("Enter liters: ");
+            liters = keyboard.nextDouble();
+            keyboard.nextLine();
+            milliliters = liters*1000;
+            System.out.println("Volume liters is "+ liters + " Volume milliliters is "+ milliliters);
+        }
+        if (selection==10)
+        {
+            System.out.println("Enter Milliliters: ");
+            milliliters = keyboard.nextDouble();
+            keyboard.nextLine();
+            liters = milliliters/1000;
+            System.out.println("Volume liters is "+ liters + " Volume milliliters is "+ milliliters);
+        }
     }
 }
